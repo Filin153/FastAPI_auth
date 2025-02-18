b8f4924cc50b15616e4ac52efb5cc604defed78c
Filin153 <dimons2006@yandex.ru>
2025-02-19 01:11:52 +0300

    edit FernetService

--- common/services/fernet.py
+++ common/services/fernet.py
@@ -1,3 +1,5 @@
+import json
+
 from cryptography.fernet import Fernet
 
 from common.config import settings
@@ -9,8 +11,14 @@ class FernetService:
     def __init__(self):
         self.__fernet = Fernet(self.__FERNET_KEY)
 
-    async def encrypt_data(self, data: str) -> bytes:
-        return self.__fernet.encrypt(data.encode())
+    async def encrypt_data(self, data: str | dict, to_json: bool = False) -> bytes:
+        if to_json:
+            return self.__fernet.encrypt(json.dumps(data).encode())
+        else:
+            return self.__fernet.encrypt(data.encode())
 
-    async def decrypt_data(self, data: bytes | str) -> str:
-        return self.__fernet.decrypt(data).decode()
+    async def decrypt_data(self, data: bytes | str, to_json: bool = False) -> str:
+        if to_json:
+            return json.loads(self.__fernet.decrypt(data).decode())
+        else:
+            return self.__fernet.decrypt(data).decode()
